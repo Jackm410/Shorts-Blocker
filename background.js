88c670403ce5f07d1a3514b8a4a0f4d898d741ab
Jack Mei <81833375+Jackm410@users.noreply.github.com>
2023-06-20 17:24:53 -0400

    Update background.js
    Added the feature where the extension close the youtube page

--- background.js
+++ background.js
@@ -5,6 +5,11 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       chrome.tabs.create(
         { url: "https://drive.google.com/file/d/1qTmt0qR_VP6geGUk1WE7IO7-ydB1HVHN/view?usp=sharing" }
       );
+
+      chrome.tabs.getCurrent(function(tab) {
+        chrome.tabs.remove(tabId, function(){});
+    });
+
     }
   });
 
